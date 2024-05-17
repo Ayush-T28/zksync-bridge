2c9945f5b3ff675288fb54b0b8e43bd05e61dc6c
Ayush Trivedi <ayush.trivedi@zeeve.io>
2024-05-17 14:58:59 +0530

    fix: update meta

--- data/meta.ts
+++ data/meta.ts
@@ -1,18 +1,18 @@
 export const portal = {
-  title: "zkSync Portal | View balances, transfer and bridge tokens",
+  title: "Memento Portal | View balances, transfer and bridge tokens",
   description:
-    "zkSync Portal allows you to view your balances, transfer tokens and bridge assets between zkSync and Ethereum",
+    "Memento Portal allows you to view your balances, transfer tokens and bridge assets between Memento and Ethereum",
   previewImg: {
-    src: "https://portal.zksync.io/preview.png",
-    alt: "zkSync Portal",
+    src: "https://mementoblockchain.com/wp-content/uploads/2022/08/Logo-reverse@2x.png",
+    alt: "Memento Portal",
   },
 };
 export const bridge = {
-  title: "zkSync Bridge | Transfer funds between zkSync and Ethereum",
+  title: "Memento Bridge | Transfer funds between Memento and Ethereum",
   description:
-    "With the zkSync Bridge you can easily deposit tokens to zkSync. Enjoy faster, cheaper and more efficient transactions with the future proof zkEVM scaling Ethereum's security and values.",
+    "With the Memento Bridge you can easily deposit tokens to Memento. Enjoy faster, cheaper and more efficient transactions.",
   previewImg: {
-    src: "https://portal.zksync.io/preview.png",
-    alt: "zkSync Bridge",
+    src: "https://mementoblockchain.com/wp-content/uploads/2022/08/Logo-reverse@2x.png",
+    alt: "Memento Bridge",
   },
 };
